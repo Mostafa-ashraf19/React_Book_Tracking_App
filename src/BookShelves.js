a75c6c6120eafbf922c1921d5fe93bb562082480
Mostafa-Ashraf19 <mostaphaashraf19@gmail.com>
2021-02-18 01:15:44 +0200

    add more shelves

--- src/BookShelves.js
+++ src/BookShelves.js
@@ -3,21 +3,31 @@ import Shelf from './Shelf'
 
 export default class BookShelves extends Component{
     shelvesNames = [{'currentlyReading':'Currently Reading'},
-    {'wantToRead':'Want To Read'},{'read':'Read'}];
+    {'wantToRead':'Want To Read'},{'read':'Read'},{'none':'None'}];
 
     filterByShelf = (shelfname) => (
         this.props.Books.filter((book)=>(
             book.shelf === shelfname
         ))
     );
-
+    _handleShelves(name){
+        const books = this.filterByShelf(Object.keys(name)[0])
+        if(books.length > 0)
+            return <Shelf key={Object.keys(name)[0]} shelfTitle={name[Object.keys(name)[0]]} changeShelf={this.props.updateShelf} booksList={books} />         
+    }
     render(){
         return (
             <div>
                 {
-                    this.shelvesNames.map((name) => (
-                        <Shelf key={Object.keys(name)[0]} shelfTitle={name[Object.keys(name)[0]]} changeShelf={this.props.updateShelf} booksList={this.filterByShelf(Object.keys(name)[0])} /> 
-                    ))
+                    
+                this.shelvesNames.map(name => this._handleShelves(name))
+
+                // when use a method down, it's give me warning, WHY??
+                // this.shelvesNames.map(shelfName => {
+                //             const books = this.filterByShelf(Object.keys(shelfName)[0])
+                //             if(books.length > 0)
+                //               return <Shelf key={Object.keys(shelfName)[0]} shelfTitle={shelfName[Object.keys(shelfName)[0]]} changeShelf={this.props.updateShelf} booksList={books} />  
+                //     })
                 }
             </div>
         )
