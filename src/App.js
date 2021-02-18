00f876277c1a33c9ddaea91eec9f33acb59d7de2
Mostafa-Ashraf19 <mostaphaashraf19@gmail.com>
2021-02-18 03:58:50 +0200

    add bind search component with gallery

--- src/App.js
+++ src/App.js
@@ -2,50 +2,39 @@ import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 import BookGallery from './BookGallery'
+import {Route,Link} from 'react-router-dom'
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
-
+  
   render() {
     return (
       <div className="app">
-        {/* <Book info={{title:'Joureny to the earth',Author:'Sasa',Image:bookLink,Categoriy:' ',Status:' '}}/> */}
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
-              <div className="search-books-input-wrapper">      
-                <input type="text" placeholder="Search by title or author"/>
+
+        <Route exact path='/' render={()=>(
+            
+              <div className="list-books">
+                <div className="list-books-title">
+                  <h1>MyReads</h1>
+                </div>
+                <div className="list-books-content">
+                  <div>
+                      <BookGallery  search={false}/>
+                  </div>
+                </div>
+                <div className="open-search">
+                  {/* <button onClick={() => )}>Add a book</button> */}
+                  <Link id='Search' to='/Search'>Add a book</Link>                  
+                </div>
               </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-            <BookGallery search={true}/>
-          </div>
-        ) : (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                  <BookGallery  search={false}/>
-              </div>
-            </div>
-            <div className="open-search">
-              <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
-            </div>
-          </div>
-        )}
+
+        )}/>
+
+        <Route  exact path='/Search' render={()=>(
+            <BookGallery  search={true}/>
+        )}/>
+
+
+        
       </div>
     )
   }
